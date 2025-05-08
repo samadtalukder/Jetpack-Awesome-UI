ee25ec8fb92381cfb86dfdd307b18541d928524b
Samad Talukder <samadtalukdar6@gmail.com>
2025-05-08 15:49:05 +0800

    Refactored: TextField defaults colors, autoCorrectEnabled

--- app/src/main/java/com/samad_talukder/jetpackcanvas/ui/components/CustomEditText.kt
+++ app/src/main/java/com/samad_talukder/jetpackcanvas/ui/components/CustomEditText.kt
@@ -1,19 +1,16 @@
-package com.samad_talukder.jetpackcanvas.components
+/**
+ *  Copyright (C) 2025. Samad Talukder (https://github.com/samadtalukder/)
+ */
+package com.samad_talukder.jetpackcanvas.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
@@ -21,7 +18,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomEditText(
     value: String,
@@ -33,8 +29,6 @@ fun CustomEditText(
     autoCorrect: Boolean = false,
     isSingleLine: Boolean = false,
     containerColor: Color,
-    textColor: Color,
-    placeholderColorColor: Color,
     roundedCornerShape: RoundedCornerShape,
     visualTransformation: VisualTransformation = VisualTransformation.None,
     trailingIcon: @Composable (() -> Unit)? = null,
@@ -47,22 +41,22 @@ fun CustomEditText(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 20.dp),
-        colors = TextFieldDefaults.textFieldColors(
-            containerColor = containerColor,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = containerColor,
+            unfocusedContainerColor = containerColor,
+            disabledContainerColor = containerColor,
+            errorContainerColor = containerColor,
             focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            textColor = textColor,
-            placeholderColor = placeholderColorColor
+            unfocusedIndicatorColor = Color.Transparent
         ),
         shape = roundedCornerShape,
         keyboardOptions = KeyboardOptions(
             keyboardType = keyboardType,
             imeAction = imeAction,
-            autoCorrect = autoCorrect,
+            autoCorrectEnabled = autoCorrect,
         ),
         visualTransformation = visualTransformation,
         placeholder = { Text(text = placeHolderText) },
         trailingIcon = { trailingIcon?.invoke() },
-
     )
 }
