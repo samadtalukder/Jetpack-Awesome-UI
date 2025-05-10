31c8fd04b4b05db487086f336d064c1b1231a401
Samad Talukder <samadtalukdar6@gmail.com>
2025-05-10 15:50:07 +0800

    Refactored: New fonts added in CustomText file

--- app/src/main/java/com/samad_talukder/jetpackcanvas/ui/components/CustomText.kt
+++ app/src/main/java/com/samad_talukder/jetpackcanvas/ui/components/CustomText.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
-import com.samad_talukder.jetpackcanvas.ui.theme.GilroyFont
 import com.samad_talukder.jetpackcanvas.ui.theme.NunitoFont
 
 @Composable
